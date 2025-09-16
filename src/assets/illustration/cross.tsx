8187866d174d0c3568a804ac312da698e1b3521e
melwin <114172287+itzTedx@users.noreply.github.com>
2025-09-16 18:45:39 +0400

    feat(home): add FlickeringGrid component for enhanced visual effects in WhyChoose section

---
+++ src/assets/illustration/cross.tsx
@@ -0,0 +1,21 @@
+export const Cross = (props: SVGProps) => {
+  return (
+    <svg {...props} fill="none" height="140" viewBox="0 0 293 140" width="293" xmlns="http://www.w3.org/2000/svg">
+      <path d="M222.5 0V140" stroke="#13098E" strokeOpacity="0.25" />
+      <path d="M292.5 70L0.499992 70" stroke="url(#paint0_linear_87_219)" strokeOpacity="0.25" />
+      <defs>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint0_linear_87_219"
+          x1="292.5"
+          x2="1.50001"
+          y1="70.5"
+          y2="70.5074"
+        >
+          <stop stopColor="#13098E" />
+          <stop offset="1" stopColor="#13098E" stopOpacity="0" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
--- src/modules/home/why-choose.tsx
+++ src/modules/home/why-choose.tsx
@@ -1,11 +1,11 @@
 import { Badge, BadgeDot } from "@/components/ui/badge";
+import { FlickeringGrid } from "@/components/ui/flicker-grid";
 import { NumberCounter } from "@/components/ui/number-counter";
 
 import { IconCommunity } from "@/assets/icons/community";
 import { IconExperience } from "@/assets/icons/experience";
 import { IconGlobe } from "@/assets/icons/globe";
 import { AboutRive } from "@/assets/pattern/about-rive";
-import { CardPattern } from "@/assets/pattern/card-pattern";
 
 import { HOW_WORKS } from "@/data/how-works";
 
@@ -37,7 +37,7 @@ export const WhyChoose = () => {
       </div>
       <div className="container relative z-10 grid max-w-7xl grid-cols-1 items-center gap-10 sm:gap-12 md:grid-cols-2 md:gap-16">
         <ul className="space-y-4 sm:space-y-5">
-          <li className="flex items-center justify-between rounded-3xl bg-linear-140 from-50% from-background to-120% to-primary-600 p-6 sm:p-7 md:from-40% md:to-110% md:p-8">
+          <li className="relative flex items-center justify-between rounded-3xl bg-linear-140 from-50% from-background to-120% to-primary-600 p-6 sm:p-7 md:from-40% md:to-110% md:p-8">
             <div>
               <p className="font-medium font-mono text-4xl text-primary leading-none sm:text-5xl">
                 <strong>
@@ -48,15 +48,19 @@ export const WhyChoose = () => {
             </div>
             <div
               aria-hidden
-              className="flex size-16 shrink-0 items-center justify-center rounded-full bg-radial-[at_50%_0%] from-background to-background/0 text-primary shadow-[0_-2.5px_4px_0_oklch(1_0_0_/_15%)_inset] sm:size-20"
+              className="relative z-10 flex size-16 shrink-0 items-center justify-center rounded-full bg-radial-[at_50%_0%] from-background to-background/0 text-primary shadow-[0_-2.5px_4px_0_oklch(1_0_0_/_15%)_inset] backdrop-blur-md sm:size-20"
             >
               <IconExperience className="size-10 md:size-12" />
             </div>
+            <div className="absolute top-0 right-0 h-full w-3/4">
+              <div className="absolute top-0 right-1/4 h-full w-px translate-x-8 bg-primary-600/25" />
+              <div className="-translate-y-1/2 absolute top-1/2 h-px w-full bg-gradient-to-l from-primary-600/25 to-transparent" />
+            </div>
           </li>
-          <li className="-bg-linear-140 flex items-center justify-between rounded-3xl from-50% from-background to-120% to-primary-600 p-6 sm:p-7 md:from-40% md:to-110% md:p-8">
+          <li className="-bg-linear-140 relative flex items-center justify-between rounded-3xl from-50% from-background to-120% to-primary-600 p-6 sm:p-7 md:from-40% md:to-110% md:p-8">
             <div
               aria-hidden
-              className="flex size-16 shrink-0 items-center justify-center rounded-full bg-radial-[at_50%_0%] from-background to-background/0 text-primary shadow-[0_-2.5px_4px_0_oklch(1_0_0_/_15%)_inset] sm:size-20"
+              className="relative z-10 flex size-16 shrink-0 items-center justify-center rounded-full bg-radial-[at_50%_0%] from-background to-background/0 text-primary shadow-[0_-2.5px_4px_0_oklch(1_0_0_/_15%)_inset] backdrop-blur-md sm:size-20"
             >
               <IconGlobe className="size-10 md:size-12" />
             </div>
@@ -70,8 +74,12 @@ export const WhyChoose = () => {
                 Businesses served across the globe
               </h3>
             </div>
+            <div className="absolute top-0 left-0 h-full w-3/4">
+              <div className="-translate-x-8 absolute top-0 left-1/4 h-full w-px bg-primary-600/25" />
+              <div className="-translate-y-1/2 absolute top-1/2 h-px w-full bg-gradient-to-r from-primary-600/25 to-transparent" />
+            </div>
           </li>
-          <li className="flex items-center justify-between rounded-3xl bg-linear-140 from-50% from-background to-120% to-primary-600 p-6 sm:p-7 md:from-40% md:to-110% md:p-8">
+          <li className="relative flex items-center justify-between rounded-3xl bg-linear-140 from-50% from-background to-120% to-primary-600 p-6 sm:p-7 md:from-40% md:to-110% md:p-8">
             <div>
               <p className="font-medium font-mono text-4xl text-primary leading-none sm:text-5xl">
                 <strong>
@@ -84,9 +92,13 @@ export const WhyChoose = () => {
             </div>
             <div
               aria-hidden
-              className="flex size-16 shrink-0 items-center justify-center rounded-full bg-radial-[at_50%_0%] from-background to-background/0 text-primary shadow-[0_-2.5px_4px_0_oklch(1_0_0_/_25%)_inset] sm:size-20"
+              className="relative z-10 flex size-16 shrink-0 items-center justify-center rounded-full bg-radial-[at_50%_0%] from-background to-background/0 text-primary shadow-[0_-2.5px_4px_0_oklch(1_0_0_/_25%)_inset] backdrop-blur-md sm:size-20"
             >
               <IconCommunity className="size-10 md:size-12" />
+            </div>
+            <div className="absolute top-0 right-0 h-full w-3/4">
+              <div className="absolute top-0 right-1/4 h-full w-px translate-x-8 bg-primary-600/25" />
+              <div className="-translate-y-1/2 absolute top-1/2 h-px w-full bg-gradient-to-l from-primary-600/25 to-transparent" />
             </div>
           </li>
         </ul>
@@ -136,7 +148,20 @@ export const WhyChoose = () => {
                 <p className="text-muted-foreground text-sm sm:text-base">{description}</p>
               </div>
               <Illustration />
-              <CardPattern aria-hidden={true} className="absolute top-0" />
+              <div className="-translate-1/2 absolute top-0 left-1/2 size-80 bg-radial from-accent/70 to-80% to-primary-950/0 blur-3xl" />
+              <div className="absolute top-0 left-0 h-[300px] w-full md:h-[600px]">
+                <FlickeringGrid
+                  className="inset-0 z-1 [mask-image:radial-gradient(160px_circle_at_top,white,transparent)]"
+                  color="#8c86f9"
+                  flickerChance={0.5}
+                  gridGap={4}
+                  height={200}
+                  maxOpacity={0.5}
+                  squareSize={2}
+                  width={400}
+                />
+              </div>
+              {/* <CardPattern aria-hidden={true} className="absolute top-0" /> */}
             </article>
           ))}
         </div>
